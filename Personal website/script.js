e45da077cf80321ac602939e690b2593d19944ff
Kaveesh Yoshitha <yoshithakaveesh83@gmail.com>
2024-01-10 22:12:17 +0530

    Personal Website Project Added

---
+++ Personal website/script.js
@@ -0,0 +1,13 @@
+function skillQualification() {
+  let displaQualification = document.getElementById("qualification-display");
+  let skills = document.getElementById("skills");
+  displaQualification.innerHTML = (
+    <ul>
+      <li>Bootstrap</li>
+    </ul>
+  );
+}
+
+function educationQualification() {}
+
+function certificationsQualification() {}
